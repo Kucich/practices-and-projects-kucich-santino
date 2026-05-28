6a7bc7145be4c8c92273a9c0eafbd2f99cc3166b
Kucich <santino.kucich@alumnos.ispi4038.edu.ar>
2026-05-28 10:54:23 -0300

    Borrado de espacios y subida de archivos

--- Practica_Programacion_II/obj/Debug/net10.0/Practica_de_Programacion.AssemblyInfo.cs
+++ Practica_Programacion_II/obj/Debug/net10.0/Practica_de_Programacion.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practica_de_Programacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09fd6609913baed700b1efef3b9d39781909ef08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b248f7838c73ee6364444be83b12c950c62404ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practica_de_Programacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practica_de_Programacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
